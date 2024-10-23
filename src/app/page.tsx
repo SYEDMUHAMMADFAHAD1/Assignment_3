8cf7f8c614857c6b22119bf2c1c8598aa133afe3
SYED Muhammad Fahad <161588586+SYEDMUHAMMADFAHAD1@users.noreply.github.com>
2024-10-23 10:16:24 +0500

    add

---
+++ src/app/page.tsx
@@ -0,0 +1,11 @@
+function Home() {
+  return (
+    <div>
+      <div className="bg-slate-700 flex justify-center items-center h-12 font-serif text-white-600">
+        Certified Cloud Applied Generative AI Engineer
+      </div>
+    </div>
+  );
+}
+
+export default Home;
